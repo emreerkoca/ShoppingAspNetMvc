40a5d792044d5f8317b16d16a20e119e9c5e1249
Emre Erkoca <emreerkoca@outlook.com>
2018-03-22 15:38:15 +0300

    Shop front and Cart front done

---
+++ ShoppingAspNetMvc/Controllers/PagesController.cs
@@ -0,0 +1,90 @@
+﻿using ShoppingAspNetMvc.Models.Data;
+using ShoppingAspNetMvc.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingAspNetMvc.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Index(string page = "")
+        {
+            // Get/set page slug
+            if (page == "")
+                page = "home";
+
+            // Declare model and DTO
+            PageVM model;
+            PageDTO dto;
+
+            // Check if page exists
+            using (Db db = new Db())
+            {
+                if (!db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new { page = "" });
+                }
+            }
+
+            // Get page DTO
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            // Set page title
+            ViewBag.PageTitle = dto.Title;
+
+            // Check for sidebar
+            if (dto.HasSidebar == true)
+            {
+                ViewBag.Sidebar = "Yes";
+            }
+            else
+            {
+                ViewBag.Sidebar = "No";
+            }
+
+            // Init model
+            model = new PageVM(dto);
+
+            // Return view with model
+            return View(model);
+        }
+
+        public ActionResult PagesMenuPartial()
+        {
+            //Declare a list of PageVM
+            List<PageVM> pageVMList;
+
+            //Get all pages except home
+            using (Db db = new Db())
+            {
+                pageVMList = db.Pages.ToArray().OrderBy(x => x.Sorting).Where(x => x.Slug != "home").Select(x => new PageVM(x)).ToList();
+            }
+
+            //Return partial view
+            return PartialView(pageVMList);
+        }
+
+        public ActionResult SidebarPartial()
+        {
+            //Declare model
+            SidebarVM model;
+
+            //init model
+            using (Db db = new Db())
+            {
+                SidebarDTO dto = db.Sidebar.Find(1);
+
+                model = new SidebarVM(dto);
+            }
+            //return partialView with model
+            return PartialView(model);
+        }
+    }
+}
